1a18e9f44d95858edae49e76fc0c2d6c86201d1f
Stefan Marx <smarx@marxworld.net>
2015-11-02 17:08:31 +0100

    Handling of 4.1 Auth

--- agent/startSandbox.sh
+++ agent/startSandbox.sh
@@ -2,8 +2,10 @@
 JAVA_OPTIONS="-Dappdynamics.agent.applicationName=APPDEX_LBG
 -Dappdynamics.controller.hostName=${controllerHost-localhost}
 -Dappdynamics.controller.port=${controllerPort-8090}
--Dappdynamics.agent.tierName=APPDEX
--Dappdynamics.agent.nodeName=NodeUTIL
+-Dappdynamics.agent.tierName=APPDEX2
+-Dappdynamics.agent.nodeName=NodeUTIL22
+-Dappdynamics.agent.uniqueHostId=NOPE222
+-Dappdynamics.agent.maxMetrics=1000
 -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 
 java $JAVA_OPTIONS -jar ./build/sandbox/agent/machineagent.jar
